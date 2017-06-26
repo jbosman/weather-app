bb66bc69f0238b808c424014b8f748ef5045e5e5
Joe Bosman <joseph.a.bosman@gmail.com>
2017-06-26 18:13:24 -0400

    Added outline for selected-day component.

--- browser/react/app/index.js
+++ browser/react/app/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+// Components
+import SelectectedDay from '../selected-day';
+
 require('./index.scss');
 
 export default function(){
 	return (
 		<div className='app-component'>
-			<h1>Hello, React Template</h1>
+			<SelectectedDay></SelectectedDay>
 		</div>
 	)
 }
---
+++ browser/react/selected-day/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+require('./index.scss');
+
+export default function SelectedDay(props){
+	return (
+		<div className='selected-day'>
+			<img src='https://fillmurray.com/200/200' alt='Weather Image' />
+			<h3>Date Info Component</h3>
+			<h3>Weather Data Component</h3>
+		</div>
+	)
+
+}
