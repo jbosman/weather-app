b56211d81934f93e86cd610ea6a111b5c75eda5c
Joe Bosman <joseph.a.bosman@gmail.com>
2017-07-18 17:18:48 -0400

    Added api keys to production env object.

--- server/env/production.js
+++ server/env/production.js
@@ -1,6 +1,7 @@
 module.exports = {
-	SESSION_SECRETE: 	process.env.SESSION_SECRETE,
-	WEATHER_API_KEY: 	process.env.DARK_SKY_API_KEY,
+	SESSION_SECRETE: 		process.env.SESSION_SECRETE,
+	WEATHER_API_KEY: 		process.env.DARK_SKY_API_KEY,
+	GOOGLE_MAPS_API_KEY: 	process.env.GOOGLE_MAPS_API_KEY,
 	LOGGING: true,
 	NATIVE: true
 }
