817eec7b152ce8f12492e06531f9ffd118650a9f
Joe Bosman <joseph.a.bosman@gmail.com>
2017-07-18 16:35:54 -0400

    Adding missing comma to projuction env object.

--- server/env/production.js
+++ server/env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
 	SESSION_SECRETE: 	process.env.SESSION_SECRETE,
-	WEATHER_API_KEY: 	process.env.DARK_SKY_API_KEY
+	WEATHER_API_KEY: 	process.env.DARK_SKY_API_KEY,
 	LOGGING: true,
 	NATIVE: true
 }
